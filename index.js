1809efd48c5507d0e5f362126eb36dc87c2e4828
Logan McAnsh <logan@mcan.sh>
2019-08-22 18:03:45 -0400

    add 'allowParens: avoid' to config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ module.exports = {
   useTabs: false,
   semi: true,
   singleQuote: true,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  proseWrap: 'always',
+  proseWrap: "always",
+  arrowParens: "avoid"
 };
